102da3a56c1be5f6c01343a853bb0c267abc7205
sosuisen <hidekazu.kubota@gmail.com>
2023-04-07 16:34:35 +0900

    fix: format

--- src/main/java/TestServlet2.java
+++ src/main/java/TestServlet2.java
@@ -1,5 +1,3 @@
-
-
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -9,20 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class TestServlet2
- */
 @WebServlet("/test2")
 public class TestServlet2 extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TestServlet2() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 	
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html; charset=UTF-8");
