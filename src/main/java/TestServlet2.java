dedd2c6891eaf3780a12430d47c112f00d55f303
sosuisen <hidekazu.kubota@gmail.com>
2023-04-07 15:49:29 +0900

    feat: add answer for kadai hatten a

---
+++ src/main/java/TestServlet2.java
@@ -0,0 +1,49 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet2
+ */
+@WebServlet("/test2")
+public class TestServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("""
+				<html><head></head>
+				<body>
+				PUT is called!
+				</body></html>
+				""");
+	}
+	
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("""
+				<html><head></head>
+				<body>
+				DELETE is called!
+				</body></html>
+				""");
+	}
+
+}
